068e29cec796d8dfcea63c3446e465e8de9865f6
shivani23212 <65727348+shivani23212@users.noreply.github.com>
2021-06-26 21:34:56 +0100

    Version 3
    Added stopwatch functionality.

--- script.js
+++ script.js
@@ -1,8 +1,109 @@
+//STOPWATCH----------------------------------------------------------------
+var toggleBtn = document.querySelector(".toggle-btn");
+var title = document.getElementById("title");
+var timerBox = document.getElementById("timerBox");
+var stopwatchDisplay = document.getElementById("stopwatch");
+var stopwatchButtons = document.getElementById("buttons");
+
+function toggleMe() {
+    toggleBtn.classList.toggle('active');
+    if (title.innerHTML == "Countdown Timer") {
+        title.innerHTML = "Stopwatch";
+        timerBox.style.display = "none";
+        stopwatchDisplay.style.display = "block";
+        stopwatchButtons.style.display = "block";
+    }
+    else {
+        title.innerHTML = "Countdown Timer";
+        timerBox.style.display = "block";
+        stopwatchDisplay.style.display = "none";
+        stopwatchButtons.style.display = "none";
+    }
+}
+
+const timer = document.getElementById("stopwatch");
+
+var hour = 0;
+var minute = 0;
+var second = 0;
+var stoptime = true;
+
+function startStopwatch() {
+    if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+
+function stopStopwatch() {
+    if (stoptime == false) {
+        stoptime = true;
+    }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+        second = parseInt(second);
+        minute = parseInt(minute);
+        hour = parseInt(hour);
+
+        second++;
+
+        if (second == 60) {
+            minute ++;
+            second = 0
+        }
+
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+
+        if (second < 10 || second == 0) {
+            second = '0' + second;
+          }
+        if (minute < 10 || minute == 0) {
+        minute = '0' + minute;
+        }
+        if (hour < 10 || hour == 0) {
+        hour = '0' + hour;
+        }
+
+        timer.innerHTML = hour+":"+minute+":"+second;
+
+        setTimeout("timerCycle()", 1000);
+    }
+}
+
+function resetTimer() {
+    timer.innerHTML = "00:00:00";
+    stoptime=true;
+    hour = 0;
+    second = 0;
+    minute = 0;
+}
+
+
+
+//TIMER----------------------------------------------------------------------
 // name parts of DOM
 const startBtn = document.getElementById("start");
 const hourDisplay = document.getElementById("hours");
 const minuteDisplay = document.getElementById("minutes");
 const secondDisplay = document.getElementById("seconds");
+var mp3_url = 'https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3';
+var myAudio = new Audio(mp3_url);
+myAudio.loop = true;
+
+function stopTimer() {
+    if (startBtn.innerHTML == "Stop Timer") {
+        myAudio.pause();
+        startBtn.innerHTML = "Start Timer";
+        startBtn.onclick=convertToSeconds;
+    }
+}
+
 
 function startTimer(timeInSeconds, callback) {
     var time = timeInSeconds;
@@ -21,21 +122,27 @@ function startTimer(timeInSeconds, callback) {
         minuteDisplay.value = minutes;
         secondDisplay.value = seconds;
 
-        if (--time < 0 && time!=undefined) { // if at 00:00:00 then call callback function (if exists)
+        if (--time < 0) { // if at 00:00:00 then call callback function (if exists)
             clearInterval(myInterval);
-        
             if (callback) {
                 callback();
-            }
+                }
         }
     }, 1000);
 }
 
 function convertToSeconds() {
+
     var seconds = (hourDisplay.value*3600)+(minuteDisplay.value*60)+secondDisplay.value;
 
-    startTimer(seconds, function() {alert("done")});
+    startTimer(seconds, function() {
+        myAudio.play();
+        startBtn.innerHTML = "Stop Timer";
+        startBtn.onclick=stopTimer;
+    });
 }
 
 startBtn.onclick=convertToSeconds;
+
+
 
